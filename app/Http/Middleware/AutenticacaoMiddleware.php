1ba622fc564919dc44029529543981bcc859f347
G22 <94983399+ASCland4@users.noreply.github.com>
2025-05-14 09:48:18 -0300

    Update AutenticacaoMiddleware.php

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AutenticacaoMiddleware
 {
@@ -11,16 +12,17 @@ class AutenticacaoMiddleware
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param  string  $metodo_autenticacao
+     * @param  string  $perfil
      * @return mixed
      */
-    public function handle($request, Closure $next, $metodo_autenticacao, $perfil) {
-        session_start();
-
-        if(isset($_SESSION['email']) && $_SESSION['email'] != '') {
+    public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
+    {
+        if (Auth::check()) {
+            // Optionally, check user role or profile here if needed
             return $next($request);
         } else {
             return redirect()->route('site.login', ['erro' => 2]);
         }
-        
     }
 }
